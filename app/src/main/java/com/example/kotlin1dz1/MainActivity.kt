ba8559c5803563a5da79af6eabafb9819feaf89f
Ainazik-bit <pulatova4410@gmail.com>
2021-06-01 16:45:34 +0600

    foractivityresult

--- app/src/main/java/com/example/kotlin1dz1/MainActivity.kt
+++ app/src/main/java/com/example/kotlin1dz1/MainActivity.kt
@@ -1,37 +1,57 @@
 package com.example.kotlin1dz1
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_second.*
 
 class MainActivity : AppCompatActivity() {
 
+    val resultCallback =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult? ->
+                if (result?.resultCode == Activity.RESULT_OK && result.data != null) {
+                    val str = result.data!!.getStringExtra("result")
+                    edit_text.setText(str)
+                } else {
+                    Toast.makeText(this, "Ошибка", Toast.LENGTH_SHORT).show()
+                }
+            }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        openActivity()
-        getData()
-    }
-    private fun openActivity(){
+//        openActivity()
+//        getData()
         button.setOnClickListener(View.OnClickListener {
-            if (edit_text.text.isNotEmpty()){
-            val fromTextView = edit_text.text.toString()
-            val intent = Intent(this, SecondActivity::class.java).apply {
-               putExtra("fromTextView", fromTextView) }
-            startActivity(intent)
-            }else{
-                Toast.makeText(this, "Поле не может быть пустым", Toast.LENGTH_SHORT).show()
-            }
-
+            openActivity()
         })
     }
-    private fun getData(){
-        val intent = intent
-        val textSecond = intent.getStringExtra("fromMain")
-        text_view1.text = textSecond
+
+
+    private fun openActivity() {
+
+        if (edit_text.text.isNotEmpty()) {
+            val intent = Intent(this, SecondActivity::class.java)
+            val fromTextView = edit_text.text.toString()
+
+            intent.putExtra("fromTextView", fromTextView)
+            resultCallback.launch(intent)
+        } else {
+            Toast.makeText(this, "Поле не может быть пустым", Toast.LENGTH_SHORT).show()
+        }
+
     }
+
+
+//    private fun getData(){
+//        val intent = intent
+//        val textSecond = intent.getStringExtra("fromMain")
+//        text_view1.text = textSecond
+//    }
 }
--- app/src/main/java/com/example/kotlin1dz1/SecondActivity.kt
+++ app/src/main/java/com/example/kotlin1dz1/SecondActivity.kt
@@ -1,36 +1,46 @@
 package com.example.kotlin1dz1
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
         getData()
-        openMain()
+//        openMain()
+        button2.setOnClickListener(View.OnClickListener {
+            openMain()
+            //Toast.makeText(this, "Поле не может быть пустым", Toast.LENGTH_SHORT).show()
+        })
     }
     private fun getData(){
-        val intent = intent
-        val textMain = intent.getStringExtra("fromTextView")
-        text_view.text = textMain
+        val text = intent.getStringExtra("fromTextView")
+        text_view.setText(text)
     }
     private fun openMain(){
-        button2.setOnClickListener(View.OnClickListener {
-            if (edit_text2.text.isNotEmpty()){
-                val fromMain = edit_text2.text.toString()
-                val intent = Intent(this, MainActivity::class.java).apply {
-                    putExtra("fromMain", fromMain) }
-                startActivity(intent)
-            }else{
-                Toast.makeText(this, "Поле не может быть пустым", Toast.LENGTH_SHORT).show()
-            }
+        if (edit_text2.text.isNotEmpty()){
+            intent.putExtra("result", edit_text2.text.toString())
+            setResult(Activity.RESULT_OK, intent)
+            finish()
+        }
+        else{
+            Toast.makeText(this, "pole ne mojet byt pusty", Toast.LENGTH_SHORT).show()
+        }
 
-        })
+    }
+
+    override fun onBackPressed() {
+        setResult(Activity.RESULT_CANCELED, intent)
+        finish()
     }
 }
